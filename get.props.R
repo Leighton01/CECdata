5ce65d277127d230c0e8d26d8fc3de87fd9e0d9d
LeightonWu <LeightonWu@protonmail.com>
2026-04-13 17:41:15 +0800

    Retrieved more columns from pubchem.
    Function to automatically group by CID per tbl IPR.

--- get.props.R
+++ get.props.R
@@ -16,26 +16,32 @@ load(cid.map.cl2)
 
 group.save <- function(df){
 
-  df.comb <- df %>% group_by(CID) %>%
+#   Group results for same CID
+  df.comb <- df %>%
+    tidyr::drop_na(Result) %>% group_by(CID) %>%
     summarise(Result = paste(Result, collapse = ".\n"), .groups = "drop")
 
+  #   Add name to table
   df.comb <- df.comb %>% left_join(cid.map.cl, join_by (CID == cid))
 
-  saveRDS(df.comb, paste(get_name(df), ".RDS"))
+#   Save RDS and csv
+  filenameRDS <- paste("RDS/", deparse(substitute(df)),".RDS")
+  saveRDS(df.comb, filenameRDS)
 
-  write.csv(df.comb, paste(get_name(df), ".csv"),
+  filenameCSV <- paste("CSV/", deparse(substitute(df)),".csv")
+  write.csv(df.comb, filenameCSV,
             fileEncoding="Windows-1252", row.names = FALSE)
 
 }
 
 
-
-# Chemical Properties -----------------------------------------------------
+# Retrieve Column Data ----------------------------------------------------
+## Chemical Properties ----------------------------------------------------
 
 desc1 <- pc_sect(cid.map.cl2$cid, "Agrochemical Information", domain = "compound", verbose=F)
 desc2 <- pc_sect(cid.map.cl2$cid, "Biologic Information", domain = "compound", verbose=F)
 
-# Physical Properties -----------------------------------------------------
+## Physical Properties -----------------------------------------------------
 pp.colour <- pc_sect(cid.map.cl2$cid, "Color / Form", domain = "compound", verbose=F)
 pp.odor <- pc_sect(cid.map.cl2$cid, "Odor", domain = "compound", verbose=F)
 pp.bp <- pc_sect(cid.map.cl2$cid, "Boiling Point", domain = "compound", verbose=F)
@@ -79,120 +85,136 @@ pp <- rbind(pp.colour2, pp.odor2, pp.bp3, pp.mp3, pp.decompn2)
 pp.comb <- pp %>% group_by(CID) %>%
   summarise(Result = paste(Result, collapse = ".\n"), .groups = "drop")
 
-saveRDS(pp.comb, "pp.comb.RDS")
+# saveRDS(pp.comb, "pp.comb.RDS")
 #
 # pp.by.name <- pp.comb %>% left_join(cid.map.cl, join_by (CID == cid)) %>% glimpse
 # write.csv(pp.by.name, "pp.name.csv", fileEncoding="Windows-1252", row.names = FALSE)
 
 
 
-# Chemical Releases -------------------------------------------------------
+## Chemical Releases -------------------------------------------------------
 fate.exp <- pc_sect(cid.map.cl2$cid, "Environmental Fate/Exposure Summary",
                 domain = "compound", verbose=F)
 # One result per CID
 
-saveRDS(fate.exp, "fate.exp.RDS")
+# group.save(fate.exp)
 
 
-# Environmental Effects ---------------------------------------------------
+## Environmental Effects --------------------------------------------------
 fate <- pc_sect(cid.map.cl2$cid, "Environmental Fate",
                     domain = "compound", verbose=F)
 
-saveRDS(fate, "fate.RDS")
+# group.save(fate, "fate.RDS")
 
-# Human Health Effects ----------------------------------------------------
+## Human Health Effects ----------------------------------------------------
 signs <- pc_sect(cid.map.cl2$cid, "Signs and Symptoms",
                  domain = "compound", verbose=F)
-saveRDS(signs, "signs.RDS")
+# group.save(signs, "signs.RDS")
 
-# Sources -----------------------------------------------------------------
+## Sources -----------------------------------------------------------------
 # Chemical Use (Source)
 # Sources Facility / Location
 # Source Industry
 
-
-
-
-
-
-
-# Water Sources and Watersheds ------------------------------------------
+## Water Sources and Watersheds ------------------------------------------
 water <- pc_sect(cid.map.cl2$cid, "Environmental Water Concentrations",
         domain = "compound", verbose=F)
-saveRDS(water, "water.RDS")
+# group.save(water)
 
 # Drinking Water Sources
-drink <-
+# drink <-
 
 # Receiving Watersheds, paragraph 2
-shed <-
+# shed <-
 
 
-# Chemical Entry Points ---------------------------------------------------
+## Chemical Entry Points ---------------------------------------------------
 
-12.2.8 Environmental Fate (S2P1)
+# 12.2.8 Environmental Fate (S2P1)
 
 
-# Monitoring Requirements -------------------------------------------------
+## Monitoring Requirements -------------------------------------------------
 req <- pc_sect(cid.map.cl2$cid, "Regulatory Information",
                  domain = "compound", verbose=F)
 
-saveRDS(req, "req.RDS")
-# Removal Technologies ----------------------------------------------------
+## Removal Technologies ----------------------------------------------------
 tech <- pc_sect(cid.map.cl2$cid, "Environmental Biodegradation",
                domain = "compound", verbose=F)
 
-saveRDS(tech, "tech.RDS")
-# Safe Production ---------------------------------------------------------
+## Safe Production ---------------------------------------------------------
 safe <- pc_sect(cid.map.cl2$cid, "Storage Conditions",
                 domain = "compound", verbose=F)
-saveRDS(safe, "safe.RDS")
-# Safe Use ----------------------------------------------------------------
-use <- pc_sect(cid.map.cl2$cid, "Personal Protective Equipment",
+## Safe Use ----------------------------------------------------------------
+uses <- pc_sect(cid.map.cl2$cid, "Personal Protective Equipment",
                domain = "compound", verbose=F)
-saveRDS(use, "use.RDS")
-11.5.2 Personal Protective Equipment (PPE)
+
+## Safe Disposal -----------------------------------------------------------
+disposal <- pc_sect(cid.map.cl2$cid, "Disposal Methods",
+               domain = "compound", verbose=F)
+
+## Consumer Products -------------------------------------------------------
+
+consum <- pc_sect(cid.map.cl2$cid, "Household Products",
+                    domain = "compound", verbose=F)
 
 
-# Safe Disposal -----------------------------------------------------------
+## Exposure Routes ---------------------------------------------------------
+
+exp.routes <-  pc_sect(cid.map.cl2$cid, "Exposure Routes",
+                          domain = "compound", verbose=F)
+# 12.1.9 Exposure Routes
 
 
-11.3.2 Disposal Methods (P4(
+## Exposure Baseline -------------------------------------------------------
 
-# Consumer Products -------------------------------------------------------
-
-
-  9.1.1 Household Products
-
+exp.base <- pc_sect(cid.map.cl2$cid, "Metabolism / Metabolites",
+                          domain = "compound", verbose=F)
+# 8.3 Metabolism / Metabolites (P2)
 
 
+## Transgenerational Effects -----------------------------------------------
 
-# Exposure Routes ---------------------------------------------------------
+generation <- pc_sect(cid.map.cl2$cid, "Health Effects",
+                        domain = "compound", verbose=F)
 
+# 12.1.8 Health Effects
 
-12.1.9 Exposure Routes
+## Hormetic Effects --------------------------------------------------------
 
+carcin <- pc_sect(cid.map.cl2$cid, "Evidence for Carcinogenicity",
+                      domain = "compound", verbose=F)
 
-# Exposure Baseline -------------------------------------------------------
+## H2O Sol. ----------------------------------------------------------------
 
-
-8.3 Metabolism / Metabolites (P2)
-
-
-# Transgenerational Effects -----------------------------------------------
-
-
-
-12.1.8 Health Effects
-
-# Hormetic Effects --------------------------------------------------------
+h20 <- pc_sect(cid.map.cl2$cid, "Volatilization from Water / Soil",
+                      domain = "compound", verbose=F)
 
 
-12.1.6 Evidence for Carcinogenicity
+# Save and Combine --------------------------------------------------------
+# Group within each, save each as RDS and CSV
+list1 <- list(pp.comb = pp.comb,
+              fate.exp,
+              fate,
+              signs,
+              water,
+              req,
+              tech,
+              safe,
+              uses,
+              disposal,
+              consum,
+              exp.routes,
+              exp.base,
+              generation,
+              carcin,
+              h20)
 
+for (l in list1) {
+  if ("Result" %in% colnames(l)){
+    group.save(l)
+  }
+}
 
-
-# H2O Sol. ----------------------------------------------------------------
-
-
-12.2.13 Volatilization from Water / Soil
+class(list1)
+summary(list1)
+saveRDS(list1, "list1.RDS")
